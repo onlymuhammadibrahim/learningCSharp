279538fcd7679432fd0e057ea467525d7b5fac46
onlymuhammadibrahim <19-10666@formanite.fccollege.edu.pk>
2020-08-07 12:04:42 +0500

    Complete With Tutorial

---
+++ LearningCSharp/Assets/Arrays.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour {
+
+    public GameObject cubePrefabs;
+
+    GameObject[] cubes = new GameObject[5];
+    Color[] colours = { Color.green,Color.red,Color.blue,Color.yellow,Color.magenta};
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < cubes.Length; i++)
+        {
+            GameObject cube = Instantiate(cubePrefabs);
+            cubes[i] = cube;
+            cube.transform.position = new Vector3((i*2),0,0);
+            cubes[i].GetComponent<Renderer>().material.color = colours[i];
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ LearningCSharp/Assets/Functiions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Functiions : MonoBehaviour {
+    int hp = 100;
+    int attackDamage = 25;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void attack()
+    {
+        hp -= attackDamage;
+        Debug.Log(hp);
+    }
+    public void heal()
+    {
+        int hpx = randomGenerator();
+        hp += hpx;
+        Debug.Log(hp + "  " + hpx );
+    }
+    int randomGenerator()
+    {
+        return Random.Range(2, 10);
+    }
+}
